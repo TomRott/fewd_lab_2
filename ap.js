0cabf89a519df9f2f9f408ccd0637175000546f9
TomRott <tom.rottenfus@gmail.com>
2020-03-16 09:54:48 +0000

    message

---
+++ ap.js
@@ -0,0 +1,20 @@
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+function myAlphabetLength ()
+{
+    console.log(myAlphabet.length);
+}
+myAlphabetLength();
+var Planets = ["earth", "mars","uranus","jupiter","Venus"];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+var wowDatatypes =["a",1,1.2];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+var hello = function() {
+    return "Hello world!";
+}
+console.log(hello());
